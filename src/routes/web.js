e82785ad83169fc3d85e46b9c691d74baf9aa18c
tandat0902 <tandat.pham292@gmail.com>
2024-03-27 20:13:45 +0700

    [240327] Configure Database For Node.js

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from "express";
+import siteController from "../controllers/siteController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('Hello world');
-    })
+    router.get('/', siteController.getHomePage);
+
     return app.use('/', router);    
 }
 
